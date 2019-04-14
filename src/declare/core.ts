4dc14bf5fa0e3a239b386af4431cafc6fe0dfa07
Benjamin Davies <bentendavies@gmail.com>
2019-04-14 13:41:08 +1200

    Add mute delay

--- src/declare/core.ts
+++ src/declare/core.ts
@@ -1,5 +1,6 @@
 export interface DeclareContext {
   audioContext: AudioContext;
+  masterGain: GainNode;
 
   play(node: DeclareNode);
   muteAll();
@@ -15,25 +16,38 @@ export interface EffectNode extends DeclareNode {
 }
 
 export function context(): DeclareContext {
-  let ctx: AudioContext | undefined;
+  const audioContext = new AudioContext();
+  const masterGain = audioContext.createGain();
+  masterGain.gain.value = 0;
+  masterGain.connect(audioContext.destination);
+
   return {
     play,
     muteAll,
     unmuteAll,
-    get audioContext() {
-      return ctx || (ctx = new AudioContext());
-    }
+    audioContext,
+    masterGain
   };
 }
 
 function play(this: DeclareContext, node: DeclareNode) {
-  node.generate(this).connect(this.audioContext.destination);
+  node.generate(this).connect(this.masterGain);
+
+  this.unmuteAll();
 }
 
+const muteDelay = 0.01;
+
 function muteAll(this: DeclareContext) {
-  this.audioContext.suspend();
+  this.masterGain.gain.linearRampToValueAtTime(
+    0,
+    this.audioContext.currentTime + muteDelay
+  );
 }
 
 function unmuteAll(this: DeclareContext) {
-  this.audioContext.resume();
+  this.masterGain.gain.linearRampToValueAtTime(
+    1,
+    this.audioContext.currentTime + muteDelay
+  );
 }
