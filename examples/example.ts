5257a06cb23a0114297226bd7661367723d0a553
Benjamin Davies <bentendavies@gmail.com>
2019-04-15 13:14:48 +1200

    Make code more readable

--- examples/example.ts
+++ examples/example.ts
@@ -6,21 +6,14 @@ import { filter, gain, osc } from '../src/declare/nodes';
 const ctx = context();
 let playing = false;
 
+const oscs = [2, 4, 5, 6, 8].map(n => osc(110 * n, 'square'));
+const demo = gain(0.1, filter(500, 1, 'lowpass', ...oscs));
+
 $('#play').click(() => {
   if (playing) {
     ctx.unmuteAll();
   } else {
-    ctx.play(
-      gain(
-        0.1,
-        filter(
-          500,
-          1,
-          'lowpass',
-          ...[2, 4, 5, 6, 8].map(n => osc(110 * n, 'square'))
-        )
-      )
-    );
+    ctx.play(demo);
     playing = true;
   }
 });
