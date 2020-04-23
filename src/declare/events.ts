fd21cfc10f8a7b4acadfa3a89c79ebafb9c1495b
Benjamin-Davies <bentendavies@gmail.com>
2020-04-23 16:24:26 +1200

    Remove unused imports

--- src/declare/events.ts
+++ src/declare/events.ts
@@ -1,6 +1,3 @@
-import { Observable, OperatorFunction } from 'rxjs';
-import { concatMap, map } from 'rxjs/operators';
-
 export interface AudioEvent { time: number; }
 export type ValueAudioEvent = AudioEvent & { value: number };
 export type BoolAudioEvent = AudioEvent & { on: boolean };
