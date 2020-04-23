f94af10667c479ed7d2a436f589bd283ec55f79e
Benjamin-Davies <bentendavies@gmail.com>
2020-04-23 16:24:36 +1200

    Move param boilerplate to shared class

--- src/declare/param/index.ts
+++ src/declare/param/index.ts
@@ -1,8 +1,37 @@
-import { SubscriptionLike } from 'rxjs';
+import { Observable, SubscriptionLike } from 'rxjs';
+
+import { resumeContext } from '../core';
+import { AudioEvent } from '../events';
 
 export { linear } from './linear';
 export { constant } from './constant';
 
 export interface Param {
   subscribe(param: AudioParam): SubscriptionLike;
+}
+
+export class BaseParam<E extends AudioEvent> implements Param {
+  source: Observable<E>;
+
+  constructor(source: Observable<E>) {
+    this.source = source;
+  }
+
+  subscribe(param: AudioParam): SubscriptionLike {
+    let lastTime = 0;
+    const subscription = this.source.subscribe({
+      next: event => {
+        // If the source backtracks, then overwrite
+        if (event.time < lastTime)
+          param.cancelAndHoldAtTime(event.time);
+
+        resumeContext();
+        this.next(param, event);
+        lastTime = event.time;
+      }
+    });
+    return subscription;
+  }
+
+  next(_param: AudioParam, _event: E) { }
 }
--- src/declare/param/linear.ts
+++ src/declare/param/linear.ts
@@ -1,23 +1,12 @@
 import { Observable } from 'rxjs';
 
-import { Param } from '.';
-import { resumeContext } from '../core';
+import { BaseParam } from '.';
 import { ValueAudioEvent } from '../events';
 
-export const linear = (source: Observable<ValueAudioEvent>) => ({
-  subscribe(param: AudioParam) {
-    let t = 0;
-    const subscription = source.subscribe({
-      next({ time, value }) {
-        // If the source backtracks, then overwrite
-        if (time < t)
-          param.cancelAndHoldAtTime(time);
-
-        resumeContext();
-        param.linearRampToValueAtTime(value, time);
-        t = time;
-      }
-    });
-    return subscription;
+export class LinearParam extends BaseParam<ValueAudioEvent> {
+  next(param: AudioParam, { value, time }: ValueAudioEvent) {
+    param.linearRampToValueAtTime(value, time);
   }
-} as Param);
+}
+
+export const linear = (source: Observable<ValueAudioEvent>) => new LinearParam(source);
